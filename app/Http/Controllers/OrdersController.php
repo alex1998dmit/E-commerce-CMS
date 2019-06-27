46db2215f9134004cd81555df2986bfb406a0e5c
alex1998dmit <alex1998dmit@gmail.com>
2019-06-27 13:08:32 +0300

    Add logic for storing orders

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Auth;
 use App\Product;
+use App\Order;
 use App\Http\Resources\OrdersCollection;
 use Illuminate\Http\Request;
 
@@ -16,8 +17,22 @@ class OrdersController extends Controller
         if (!$user) {
             return response()->json(['message' => 'you need to login'], 403);
         }
-        $user_orders = $user->order->pluck('product_id')->toArray();
-        $products = Product::whereIn('id', $user_orders)->paginate(5);
-        return new OrdersCollection($products);
+        $orders = $user->order;
+        // $user_orders = $user->order->pluck('product_id')->toArray();
+        // $products = Product::whereIn('id', $user_orders)->paginate(5);
+        return new OrdersCollection($orders);
+    }
+
+    public function store(Request $request)
+    {
+        // TODO обдумать систему скидок, как реализовать на сервере
+        $price = Product::find($request->product_id)->first()->price;
+        $order = Order::create([
+            'product_id' => $request->product_id,
+            'user_id' => Auth::id(),
+            'amount' => $request->amount,
+            'sum' => $price * $request->amount,
+        ]);
+        return response()->json($order, 201);
     }
 }
