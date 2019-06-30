7322b726d9a300533b64fe3de92d9cd515d8a265
alex1998dmit <alex1998dmit@gmail.com>
2019-06-30 22:54:20 +0300

    Add test authentication logic for checking user's roles

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 use App\ShoppingCart;
 use App\Category;
@@ -9,19 +10,28 @@ use App\Order;
 
 class AdminController extends Controller
 {
-    public function index()
+    public function __construct()
     {
+        $this->middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+        // TODO сделать миддлвару и засунуть в конструктор
+        $request->user()->authorizeRoles(['opertor', 'admin']);
         return view('admin.index');
     }
 
-    public function fromCarts()
+    public function fromCarts(Request $request)
     {
+        $request->user()->authorizeRoles(['opertor', 'admin']);
         $carts =ShoppingCart::all();
         return view('admin.usersCart.index')->with('carts', $carts);
     }
 
-    public function categories()
+    public function categories(Request $request)
     {
+        $request->user()->authorizeRoles(['opertor', 'admin']);
         $categories = Category::where('parent_id', '=', 0)->get();
         $allCategories = Category::all();
         return view('admin.categories.index',compact('categories','allCategories'));
