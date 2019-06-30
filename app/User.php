063eb98b7a178a881f37d5680eaa65a0fa8d437a
alex1998dmit <alex1998dmit@gmail.com>
2019-06-30 22:44:56 +0300

    Add function for checking roles to User model

--- app/User.php
+++ app/User.php
@@ -38,6 +38,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    // RELATHIONSHIPS
+
     public function wishList()
     {
         return $this->hasMany('App\WishList', 'user_id', 'id');
@@ -57,4 +59,37 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Role::class);
     }
+
+    // NOT RELATHIONSHIPS
+
+    /**
+    * @param string|array $roles
+    */
+    public function authorizeRoles($roles)
+    {
+        if (is_array($roles)) {
+            return $this->hasAnyRole($roles) || abort(401, 'This action is unauthorized.');
+        }
+
+        return $this->hasRole($roles) || abort(401, 'This action is unauthorized.');
+    }
+
+    /**
+    * Check multiple roles
+    * @param array $roles
+    */
+    public function hasAnyRole($roles)
+    {
+        return null !== $this->role()->whereIn('name', $roles)->first();
+    }
+
+    /**
+    * Check one role
+    * @param string $role
+    */
+    public function hasRole($role)
+    {
+        return null !== $this->role()->where('name', $role)->first();
+    }
+
 }
