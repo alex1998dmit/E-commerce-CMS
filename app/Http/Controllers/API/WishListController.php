4dca1283593443c021200de1ec8d73feeea2448d
alex1998dmit <alex1998dmit@gmail.com>
2019-07-08 10:51:35 +0300

    Remove API logic for wishlist to API Controller/add errors for add and index methods

---
+++ app/Http/Controllers/API/WishListController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Auth;
+use Validator;
+use App\User;
+use App\Product;
+use App\WishList;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\WishListCollection;
+
+
+class WishListController extends Controller
+{
+    public function index()
+    {
+        // TODO работать без лишних моделей, возможно через посредников
+        if (!Auth::user()) {
+            return response()->json(['message' => 'you need to login'], 401);
+        }
+        $user = Auth::user();
+        $user_wishlist = $user->wishList->pluck('product_id')->toArray();
+        $products = Product::whereIn('id', $user_wishlist)->paginate(5);
+        return new WishListCollection($products);
+    }
+
+    public function store(Request $request)
+    {
+        if (!Auth::user()) {
+            return response()->json(['message' => 'you need to login'], 401);
+        }
+
+        $rules = [
+            'product_id' => 'required|integer|min:1',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            $response['data'] = $validator->messages();
+            return $response;
+        }
+
+        $wishList = WishList::where('product_id', $request->product_id)->first();
+        if ($wishList) {
+            return response()->json(['message' => 'you already have this product at your wishList'], 406);
+        } else {
+            $wishList = WishList::create([
+                'product_id' => $request->product_id,
+                'user_id' => Auth::id(),
+            ]);
+            return response()->json($wishList, 201);
+        }
+    }
+
+    public function delete(WishList $wishList)
+    {
+        if (Auth::user()) {
+            return response()->json(['message' => 'you need to login'], 401);
+        }
+
+        $user_id = Auth::id();
+        if ($wishList->user_id === $user_id && $user_id) {
+            $wishList->delete();
+            return response()->json(null, 204);
+        }
+        return response()->json(['message' => 'Not from your wishList'], 403);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -29,29 +29,28 @@ Route::get('categories', 'API\CategoriesController@index');
     //---- Products
 Route::get('/products/{product}', 'API\ProductsController@show');
 Route::get('products', 'API\ProductsController@index');
-Route::get('user/removefromwishlist/{wishList}', 'WishListController@delete')->middleware('auth:api');
 
+Route::get('user/removefromwishlist/{wishList}', 'API\WishListController@delete')->middleware('auth:api');
 
 //-- Auth's actions
 Route::group(['middleware' => 'auth:api'], function() {
     //-- User's info
-    // Route::post('user/wishlist', 'WishListController@store');
     Route::group(['prefix' => 'user'], function() {
         Route::get('show/{id}', 'API\UserController@show');
         Route::post('logout', 'AuthenticationController@logoutAPI');
 
-    //---- WishList
-        Route::get('/wishlist', 'WishListController@index');
-        Route::post('/wishlist', 'WishListController@store');
+        //---- WishList
+        Route::get('/wishlist', 'API\WishListController@index');
+        Route::post('/wishlist', 'API\WishListController@store');
 
-    //---- Product Cart
+        //---- Product Cart
         Route::get('/cart', 'API\ShoppingCartController@index');
         Route::get('/removefromcart/{shoppingCart}', 'API\ShoppingCartController@delete');
         Route::post('/cart', 'API\ShoppingCartController@store');
         Route::post('/cart', 'API\ShoppingCartController@store');
         Route::put('/cart/{shoppingCart}', 'API\ShoppingCartController@update');
 
-    //---- Order
+        //---- Order
         Route::get('/orders', 'OrdersController@index');
         Route::post('/orders', 'OrdersController@store');
         Route::get('/orders/{order}', 'OrdersController@show');
